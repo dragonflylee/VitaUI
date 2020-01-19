b808b8853cd756f89214484f9b0fcc8cf4f7b512
dragonflylee <dragonflylee@outlook.com>
2020-01-20 01:57:34 +0800

    use freetype

--- src/font.c
+++ src/font.c
@@ -39,31 +39,26 @@ int is_latin_char(const unsigned int c)
 	return 0;
 }
 
-static vita2d_pvf *g_font;
+static vita2d_font *g_font;
 
 void font_init()
 {
-    vita2d_system_pvf_config pvf_config[] = {
-		{SCE_PVF_LANGUAGE_K, is_korean_char},
-		{SCE_PVF_LANGUAGE_C, is_chinese_char},
-		{SCE_PVF_LANGUAGE_LATIN, is_latin_char},
-		{SCE_PVF_DEFAULT_LANGUAGE_CODE, NULL}};
-	g_font = vita2d_load_system_pvf(4, pvf_config);
+	g_font = vita2d_load_font_file("sa0:/data/font/pvf/cn0.pvf");
 }
 
 void font_destroy()
 {
-    vita2d_free_pvf(g_font);
+    vita2d_free_font(g_font);
 }
 
 int font_draw(int x, int y, unsigned int color, const char *text)
 {
 	if (!g_font) return -1;
-	return vita2d_pvf_draw_text(g_font, x, y + 20, color, 1.f, text);
+	return vita2d_font_draw_text(g_font, x, y + 20, color, 20.f, text);
 }
 
 int font_width(const char *text)
 {
 	if (!g_font) return -1;
-	return vita2d_pvf_text_width(g_font, 1.f, text);
+	return vita2d_font_text_width(g_font, 20.f, text);
 }
